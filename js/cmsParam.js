8254087037735ef4c095b464081ffdc730ef86e3
Javier de Pedro López <j.de.pedro.lopez@accenture.com>
2019-03-14 13:17:50 +0100

    Auto deployment from current documentation

---
+++ js/cmsParam.js
@@ -0,0 +1,14 @@
+(function () {
+  // Document on load.
+  $(function(){
+    let params = new URLSearchParams(window.location.search);
+    let fromCms = params.get("fromCms") === "true";
+    if(fromCms){
+      $('.fixedHeaderContainer').hide();
+      $('#docsNav').hide();
+      $('.navPusher').removeClass('navPusher');
+      $('#footer').hide();
+    }
+  });
+
+}());
